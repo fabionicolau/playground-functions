dead0056704b81e6b942b38faafd5fcb9f45292d
fabionicolau <fabionicolaujr@gmail.com>
2021-12-02 20:45:01 -0300

    Requisito 11 - ok

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,8 +18,34 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  let contador = 0;
+  for (let numeros of array) {
+    if (numeros < 0 || numeros > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    for (let numbers of array) {
+      if (numeros === numbers) {
+        contador += 1;
+      }
+      if (contador >= 3) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+    }
+    contador = 0;
+  }
+  let segundaParte = [];
+  let terceiraParte = [];
+  for (let index = 2; index < 7; index += 1) {
+    segundaParte.push(array[index]);
+  }
+  for (let index = 7; index < 12; index += 1) {
+    terceiraParte.push(array[index]);
+  }
+  return `(${array[0]}${array[1]}) ${segundaParte.join('')}-${terceiraParte.join('')}`;
 }
 
 // Desafio 12
